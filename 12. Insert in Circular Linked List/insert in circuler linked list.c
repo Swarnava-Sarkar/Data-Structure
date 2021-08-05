0fc3d442d4176699191b92dc0a4f58f7aab22ec9
Swarnava Sarkar <swarnavasarkar01@gmail.com>
2021-08-05 12:27:37 +0530

    Insert operation

---
+++ 12. Insert in Circular Linked List/insert in circuler linked list.c
@@ -0,0 +1,67 @@
+//CIRCULAR LINKED LIST
+
+#include<stdio.h>
+#include<stdlib.h>
+
+typedef struct node{
+    int data;
+    struct node* next;
+}Node;
+
+void display(Node* head)
+{
+    Node* ptr = head;
+    printf("\n\n");
+    do
+    {
+        printf("%d   ",ptr->data);
+        ptr = ptr->next;
+    }while(ptr!=head);
+    printf("\n\n");
+}
+
+Node* insertNode(Node* head , int data)
+{
+    Node* ptr = (Node*)malloc(sizeof(Node));
+    ptr->data = data;
+
+    ptr->next = head->next;
+    head->next = ptr;
+
+    head = ptr;
+
+    return head;
+}
+
+int main()
+{
+    Node* head;
+    Node* first;         // making the struct node type pointers
+    Node* second;
+    Node* third;
+
+    head = (Node*)malloc(sizeof(Node));
+    first = (Node*)malloc(sizeof(Node));      // allocating memory for the nodes
+    second = (Node*)malloc(sizeof(Node));     // which is equal to one struct node
+    third = (Node*)malloc(sizeof(Node));
+
+    head->data = 1;
+    head->next = first;     // the next pointer of the head node pointing to address of 1st node
+
+    first->data = 2;
+    first->next = second;
+
+    second->data = 3;
+    second->next = third;
+
+    third->data = 4;
+    third->next = head;     // linking the last node with the first node
+
+    printf("------- The Circular Linked-List is -------");
+    display(head);
+
+    head = insertNode(head,30);
+
+    printf("------- Node Inserted -------");
+    display(head);
+}
